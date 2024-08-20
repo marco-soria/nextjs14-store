90d643bf66c962399bd1616b95bde594d8cc56a3
marco-soria <marco.soriaa7@gmail.com>
2024-08-19 20:03:05 -0500

    landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import { Hero } from "app/components/home/Hero"
+import { Description } from "app/components/home/Description"
+import { MainProducts } from "app/components/home/MainProducts"
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <h1>Hello World</h1>
+    <main>
+      <Hero />
+      <Description />
+      <MainProducts />
     </main>
-  );
+  )
 }
