4a7fad3d288419145153c9fa534071b722aed72d
marco-soria <marco.soriaa7@gmail.com>
2024-08-20 20:04:40 -0500

    fetch api

--- src/components/home/MainProducts/MainProducts.tsx
+++ src/components/home/MainProducts/MainProducts.tsx
@@ -1,7 +1,19 @@
-export const MainProducts = () => {
-    return(
-      <section>
-        <h1>Main Products</h1>
-      </section>
-    )
-  }
+const getProducts = async () => {
+  const response = await fetch(`${process.env.SHOPIFY_HOSTNAME}/admin/api/2024-07/products.json`, {
+    headers: new Headers({
+      'X-Shopify-Access-Token': process.env.SHOPIFY_API_KEY || ""
+    })
+  })
+  const data = await response.json()
+  return data
+}
+
+export const MainProducts = async () => {
+  const products = await getProducts()
+  console.log(products)
+  return (
+    <section>
+      <h1>Main Products</h1>
+    </section>
+  )
+}
