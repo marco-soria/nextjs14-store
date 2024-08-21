0efff77650e2634a8c64773a8a8fc60dcbb7ae65
marco-soria <marco.soriaa7@gmail.com>
2024-08-21 15:41:20 -0500

    data fetching

--- src/services/shopify/urls.ts
+++ src/services/shopify/urls.ts
@@ -3,5 +3,8 @@ import { env } from "app/config/env"
 export const shopifyUrls = {
   products: {
     'all': `${env.SHOPIFY_HOSTNAME}/admin/api/2024-07/products.json`
+  },
+  collections: {
+    'all': `${env.SHOPIFY_HOSTNAME}/admin/api/2024-07/smart_collections.json`
   }
 }
