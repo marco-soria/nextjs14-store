176a689966bb6ad6dbefa45c6eba27a60f5843e9
marco-soria <marco.soriaa7@gmail.com>
2024-08-21 10:52:17 -0500

    changed data fecth arq

--- src/app/(home)/template.tsx
+++ src/app/(home)/template.tsx
@@ -6,7 +6,7 @@ import { useEffect } from "react"
 export default function HomeLayout({ children }: { children: React.ReactNode }) {
   
   useEffect(() => {
-    console.log('envio de metricas')
+    
   }, [])
   
   return (
