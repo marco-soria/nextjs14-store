14234b81301f1cc88e345fb8bbfdc27ce5b28de1
marco-soria <marco.soriaa7@gmail.com>
2024-08-21 19:49:43 -0500

    errors  change in shopify doc

--- src/components/signup/NewAccountForm.tsx
+++ src/components/signup/NewAccountForm.tsx
@@ -8,22 +8,22 @@ export const NewAccountForm = () => {
   const [errors, setErrors] = useState<string[]>([]);
   const [loading, setLoading] = useState<boolean>(false);
 
-  const handleSubmit = (event: any) => {
+  const handleSubmit = async (event:any) => {
     event.preventDefault();
     const formData = new FormData(event.target);
-    handleCreateUser(formData)
+    await handleCreateUser(formData)
   }
 
   return (
     <div className={styles.NewAccountForm}  >
       <h1 className={styles.NewAccountForm__title}>New Account</h1>
       <form className={styles.NewAccountForm__form} onSubmit={handleSubmit}>
-        <input type="text" name="first_name" placeholder="Name" disabled={loading} />
-        <input type="text" name="last_name" placeholder="Lastname" disabled={loading} />
+        <input type="text" name="firstName" placeholder="Name" disabled={loading} />
+        <input type="text" name="lastName" placeholder="Lastname" disabled={loading} />
         <input type="text" name="email" placeholder="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" disabled={loading} />
         <input type="text" name="phone" placeholder="phone number" pattern="^[0-9]*$" disabled={loading} />
-        <input type="password" name="password" placeholder="password" disabled={loading} />
-        <input type="password" name="password_confirmation" placeholder="re-type password" disabled={loading} />
+        {/* <input type="password" name="password" placeholder="password" disabled={loading} />
+        <input type="password" name="password_confirmation" placeholder="re-type password" disabled={loading} /> */}
         <input type="submit" name="submit" value="Crear cuenta" disabled={loading} />
       </form>
       {errors.length > 0 && 
